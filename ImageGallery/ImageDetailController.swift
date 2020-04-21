36a97e4908748419c57211cecfb0cbbca43770dc
nattle <eliseyzubkov@gmail.com>
2020-04-21 22:21:03 +0300

    setup autolayout for keep image centred

--- ImageGallery/ImageDetailController.swift
+++ ImageGallery/ImageDetailController.swift
@@ -14,14 +14,22 @@ class ImageDetailController: UIViewController, UIScrollViewDelegate {
         set {
             imageView.image = newValue
             imageView.sizeToFit()
-            scrollView.contentSize = imageView.frame.size
+            let size = newValue?.size ?? CGSize.zero
+            scrollView.contentSize = size
+
+            scrollViewHeight?.constant = size.height
+            scrollViewWidth?.constant = size.width
+
         }
     }
+
+    private var scrollViewWidth: NSLayoutConstraint?
+    private var scrollViewHeight: NSLayoutConstraint?
 
     let scrollView: UIScrollView = {
         let scrollView = UIScrollView()
         scrollView.minimumZoomScale = 1 / 25
-        scrollView.maximumZoomScale = 1.0
+        scrollView.maximumZoomScale = 2
         return scrollView
     }()
 
@@ -29,13 +37,37 @@ class ImageDetailController: UIViewController, UIScrollViewDelegate {
         return imageView
     }
 
+    func scrollViewDidZoom(_ scrollView: UIScrollView) {
+        scrollViewWidth?.constant = scrollView.contentSize.width
+        scrollViewHeight?.constant = scrollView.contentSize.height
+    }
+
     override func viewDidLoad() {
         print(#function)
         super.viewDidLoad()
         view.addSubview(scrollView)
         scrollView.addSubview(imageView)
-        scrollView.anchor(top: view.topAnchor, left: view.leftAnchor, bottom: view.bottomAnchor, right: view.rightAnchor)
+
         scrollView.delegate = self
+        setupAutolayout()
+    }
+
+    private func setupAutolayout() {
+        print(#function)
+        scrollView.translatesAutoresizingMaskIntoConstraints = false
+
+        let imageViewSize = imageView.frame.size
+
+        scrollViewWidth = scrollView.widthAnchor.constraint(equalToConstant: imageViewSize.width)
+        scrollViewHeight = scrollView.heightAnchor.constraint(equalToConstant: imageViewSize.height)
+
+        scrollViewWidth?.isActive = true
+        scrollViewHeight?.isActive = true
+
+        NSLayoutConstraint.activate([
+            scrollView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            scrollView.centerYAnchor.constraint(equalTo: view.centerYAnchor)
+        ])
 
     }
 }
