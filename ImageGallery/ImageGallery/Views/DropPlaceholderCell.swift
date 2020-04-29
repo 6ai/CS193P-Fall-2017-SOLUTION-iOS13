9fd06e4e3ef66df8421383c67171b0ae42aa342c
nattle <eliseyzubkov@gmail.com>
2020-04-29 18:47:28 +0300

    DropPlaceholderCell.swift : setup activity indicator

--- ImageGallery/ImageGallery/Views/DropPlaceholderCell.swift
+++ ImageGallery/ImageGallery/Views/DropPlaceholderCell.swift
@@ -9,25 +9,17 @@
 import UIKit
 
 class DropPlaceholderCell: UICollectionViewCell {
-    var loadingActivityIndicator: UIActivityIndicatorView = {
-        let indicator = UIActivityIndicatorView(frame: CGRect(x: 0, y: 0, width: 40, height: 40))
-        return indicator
-    }()
+    let loadingActivityIndicator: UIActivityIndicatorView = UIActivityIndicatorView()
 
     override init(frame: CGRect) {
         super.init(frame: frame)
         addSubview(loadingActivityIndicator)
-        layoutSubviews()
+        loadingActivityIndicator.anchor(top: topAnchor, left: leftAnchor, bottom: bottomAnchor, right: rightAnchor)
+        loadingActivityIndicator.startAnimating()
+        backgroundColor = .white
     }
 
     required init?(coder: NSCoder) {
         fatalError("init(coder: ) has not been implemented")
     }
-
-    override func layoutSubviews() {
-        super.layoutSubviews()
-        loadingActivityIndicator.centerXAnchor.constraint(equalTo: centerXAnchor).isActive = true
-        loadingActivityIndicator.centerYAnchor.constraint(equalTo: centerYAnchor).isActive = true
-    }
-
 }
